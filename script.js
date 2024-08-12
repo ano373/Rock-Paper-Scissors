097ec15f985d5610fa123e3d72c52a1d901c8ad7
ano373 <andrewamin37@gmail.com>
2024-08-13 01:12:54 +0300

    finish the project no ui

---
+++ script.js
@@ -0,0 +1,68 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * 3);
+  switch(random) {
+    case 0:
+    return "Rock";
+      break;
+    case 1:
+    return "Paper";
+      break;
+    case 2:
+    return "Scissors";
+      break;
+  }
+}
+
+function getHumanChoice(){
+  const input = prompt("Choose between Rock Paper Scissor").trim().toLowerCase();
+  return input.charAt(0).toUpperCase() + input.slice(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+  if(humanChoice === computerChoice) {
+    alert("Tie");
+  } 
+  else if (humanChoice == "Scissors") {
+    if (computerChoice == "Rock") {
+      alert("Computer won");
+      computerScore++;
+    } else {
+      alert("Human won");
+      humanScore++;
+    }
+  }
+  else if (humanChoice == "Paper") {
+    if (computerChoice == "Scissors") {
+      alert("Computer won");
+      computerScore++;
+    } else {
+      alert("Human won");
+      humanScore++;
+    }
+  }
+  else if (humanChoice == "Rock") {
+    if (computerChoice == "Paper") {
+      alert("Computer won");
+      computerScore++;
+    } else {
+      alert("Human won");
+      humanScore++;
+    } 
+  }
+  
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+}
+
+playGame();
+
+alert(`Human score is ${humanScore} and Computer score is ${computerScore}`);
